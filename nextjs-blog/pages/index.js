3befdb9083e6293d2e717a4c5d2ed52b9dbe0a23
Cesar A <89220385+Cadame01@users.noreply.github.com>
2021-09-16 04:39:09 +0000

    Fixed profile

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -1,5 +1,5 @@
 import Link from 'next/link'
-import Date from '../components/date
+import Date from '../components/date'
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
